127d1a07bf393c93c32e7965a4841f0dc86a2e93
olitrum <olitrum@gmail.com>
2019-11-06 11:08:56 +0300

    7

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
-var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
@@ -15,7 +14,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(bodyParser.json());
